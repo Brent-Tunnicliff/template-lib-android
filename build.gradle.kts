3ff4c6b2af27ccc32a659bd6a7108e9f909a497a
Brent Tunnicliff <brenttunnicliff@gmail.com>
2024-09-08 03:31:20 +1000

    Removed unneeded top level declarations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
 // Copyright © 2024 Brent Tunnicliff <brent@tunnicliff.dev>
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    id("maven-publish")
-}
+plugins {}
