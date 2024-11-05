88a245f90dad3a6b994c006485132543780ed9c0
Brent Tunnicliff <brent@tunnicliff.dev>
2024-11-05 21:51:21 +1100

    Added padding to MainView

--- app/src/main/java/dev/tunnicliff/replace_me/demo/view/MainView.kt
+++ app/src/main/java/dev/tunnicliff/replace_me/demo/view/MainView.kt
@@ -2,9 +2,12 @@
 
 package dev.tunnicliff.replace_me.demo.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.tunnicliff.replace_me.demo.container.ViewModelFactory
 import dev.tunnicliff.ui.theme.ThemedPreviewer
@@ -14,7 +17,8 @@ fun MainView(
     viewModel: MainViewModel = viewModel(factory = ViewModelFactory)
 ) {
     Text(
-        text = "Value: ${viewModel.getMessage()}"
+        text = "Value: ${viewModel.getMessage()}",
+        modifier = Modifier.padding(8.dp)
     )
 }
 
